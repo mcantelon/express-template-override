b7a4cb2a89e6bd3cb0735d1e3ecd23e77196a9d9
Mike Cantelon <mcantelon@gmail.com>
2011-05-15 15:06:05 -0700

    More work on tests.

--- test/express-template-override.test.js
+++ test/express-template-override.test.js
@@ -10,23 +10,40 @@ module.exports = {
 
   'layout override': function() {
 
-    var app = require('./fixtures/test_app/app');
+    var app = require('./fixtures/two_template_override/app');
+
     assert.response(app,
       { url: '/' },
       { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
       function(res){
         assert.includes(res.body, '<title>Override title</title>');
-      });
+      }
+    );
   },
 
   'index override': function() {
     
-    var app = require('./fixtures/test_app/app');
+    var app = require('./fixtures/two_template_override/app');
+
     assert.response(app,
       { url: '/' },
       { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
       function(res){
         assert.includes(res.body, '<p>Override index.jade</p>');
-      });
+      }
+    );
+  },
+
+  'layout override only': function() {
+
+    var app = require('./fixtures/layout_template_override_only/app');
+
+    assert.response(app,
+      { url: '/' },
+      { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
+      function(res){
+        assert.includes(res.body, '<p>Normal index.jade</p>');
+      }
+    );
   }
 };
